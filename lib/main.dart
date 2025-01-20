fccd7bbf46f5c0e7ddf3f4469edfebabdb9864cd
timothyamanya <amanyatimothy2099@gmail.com>
2025-01-20 13:54:23 +0300

    changed color to light blue

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreenAccent),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Kikomando'),
